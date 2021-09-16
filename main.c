a22bffbcc3b7ab3ce3aa0b4fe86afbfac9a21480
aabraham <aram.abrahamyan2021@gmail.com>
2021-09-17 00:45:35 +0400

    first commit

--- main.c
+++ main.c
@@ -1,30 +1,5 @@
-#include <unistd.h>
-
-void ft_print(char c)
-{
-    write(1, &c, 1);
-}
-
-void ft_number(int nb)
-{
-    unsigned int nbr_unsigned;
-
-    if (nb < 0)
-    {
-       nbr_unsigned = (unsigned int)(-1*nb);
-
-    }
-   else
-      nbr_unsigned = (unsigned int)nb;
-    if(nbr_unsigned >= 10)
-    {
-        ft_number(nbr_unsigned / 10);
-        ft_number(nbr_unsigned % 10);
-    }
-    else
-       ft_print(nbr_unsigned + '0');
-}
+#include <stdio.h>
 int main()
 {
-    ft_number(500000);
+   printf("Hello World");
 }
